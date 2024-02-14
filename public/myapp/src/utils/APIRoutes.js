5928c0e221af254e90345bd5acf3076119126b92
RajdeepTiwari-24 <rajdeeptlc@gmail.com>
2024-02-14 22:03:30 +0530

    Backend Link

--- public/myapp/src/utils/APIRoutes.js
+++ public/myapp/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://pixel-red-eight.vercel.app";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const uploadimagesRoute =`${host}/api/images/upload`;
